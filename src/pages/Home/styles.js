3b9513b393ceb3fbf92f4ca3086a49f56d593295
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2023-12-03 11:26:10 -0300

    Estilizado Brand com a logo

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,8 +4,8 @@ export const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: grid;
-    grid-template-columns: 250px auto;
-    grid-template-rows: 105px 128px auto 64px;
+    grid-template-columns: 25rem auto;
+    grid-template-rows: 10.5rem 12.8rem auto 6.4rem;
     grid-template-areas: 
     "brand header"
     "menu search"
@@ -17,25 +17,40 @@ export const Container = styled.div`
 
 export const Brand = styled.div`
     grid-area: brand;
-    background-color: red;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: ${({theme}) => theme.BACKGROUND_COLOR_900};
+
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: ${({theme}) => theme.COLORS.BACKGROUND_700};
+
+    > h1 {
+        font-size: 2.4rem;
+        color: ${({theme}) => theme.COLORS.ORANGE};
+
+    }
     `
 
 export const Menu = styled.ul`
     grid-area: menu;
-    background-color: green;
+    
     `
 
 export const Search = styled.div`
     grid-area: search;
-    background-color: violet;
+    
     `
 
 export const Content = styled.div`
     grid-area: content;
-    background-color: yellow;
+    
     `
 
 export const NewNote= styled.button`
     grid-area: newnote;
-    background-color: blue;
+    
     `
