4f5427228655d681e95248b782336d285c21eca5
Marcelo Lima <marcelo.ribeiro@fiscosys.com.br>
2023-12-11 12:42:41 -0300

    Adicionado Header da page New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,10 +1,21 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 import Header from "../../components/Header";
+import Input from "../../components/Input";
 
 const New = () => {
   return (
     <Container>
       <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar Nota</h1>
+            <a href="/">Voltar</a>
+          </header>
+          <Input placeholder="Titulo" />
+        </Form>
+      </main>
     </Container>
   );
 };
