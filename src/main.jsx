1fcff78dd39612a42eee4c7396f62fb6934731e4
Marcelo Lima <marcelo.ribeiro@fiscosys.com.br>
2023-12-11 12:29:53 -0300

    criada pagina New

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
-import theme from './styles/theme'
-import GlobalStyles from './styles/global'
-import Profile from './pages/Profile'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/global";
+import New from "./pages/New";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <Profile/>
+      <GlobalStyles />
+      <New />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/New/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+import Header from "../../components/Header";
+
+const New = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default New;
