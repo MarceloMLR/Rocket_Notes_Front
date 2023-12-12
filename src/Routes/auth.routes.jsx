0553ca52adf0ee799cda380eb722fff1091f0f2e
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2023-12-11 22:30:54 -0300

    Adicionado React-router-dom

---
+++ src/Routes/auth.routes.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import New from '../pages/New';
+import Details from '../pages/Details';
+import Profile from '../pages/Profile';
+
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/new' element={<New/>}/>
+        <Route path='/Details/:id' element={<Details/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+  )
+}
+
+export default AppRoutes
