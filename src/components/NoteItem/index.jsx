a2d44111aa12e28cb73159cfa8e6a7d488054b18
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2023-12-11 22:03:52 -0300

    Estilizando NoteItem

--- src/components/NoteItem/index.jsx
+++ src/components/NoteItem/index.jsx
@@ -2,14 +2,15 @@ import { FiPlus, FiX } from "react-icons/fi";
 import { Container } from "./styles";
 
 const NoteItem = ({ isNew, value, onClick, ...rest }) => {
-  return;
+  return(
   <Container isNew={isNew}>
     <input type="text" value={value} readOnly={!isNew} {...rest} />
 
-    <button type="button" onClick={onClick}>
-      {isNew ? <FiPlus /> : <FiX />}{" "}
+    <button type="button" onClick={onClick} className={isNew ? "button-add" : "button-delete"}>
+      {isNew ? <FiPlus /> : <FiX />}
     </button>
-  </Container>;
+  </Container>
+  );
 };
 
 export default NoteItem;
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,6 +2,8 @@ import { Container, Form } from "./styles";
 import Header from "../../components/Header";
 import Input from "../../components/Input";
 import Textarea from "../../components/Textarea";
+import Section from "../../components/Section";
+import NoteItem from "../../components/NoteItem";
 
 const New = () => {
   return (
@@ -17,6 +19,11 @@ const New = () => {
           <Input placeholder="Titulo" />
 
           <Textarea placeholder="Observações" />
+          
+          <Section title="Links Uteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo Link" />
+          </Section>
         </Form>
       </main>
     </Container>
